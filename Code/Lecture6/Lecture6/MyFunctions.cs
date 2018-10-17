d757c5dd3742ad86de1b43cfe13cf71beb0c2fb0
Witold <gawlikowicz@gmail.com>
2018-10-17 12:42:38 +0100

    Updated code

---
+++ Code/Lecture6/Lecture6/MyFunctions.cs
@@ -0,0 +1,15 @@
+﻿using ExcelDna.Integration;
+
+namespace TestExcelDNA
+{
+    public static class MyFunctions
+    {
+        [ExcelFunction(Description = "My first .NET function")]
+        public static string SayHello(
+            [ExcelArgument(Description = "Input string")]
+            string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
